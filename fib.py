ba5f2ff33752f263d6d796733a1c9fde898bfddb
Vidyarmenon <64121754+Vidyarmenon@users.noreply.github.com>
2020-04-22 11:17:53 +0530

    created fib.py
    prints fibonacci series

---
+++ fib.py
@@ -0,0 +1,12 @@
+a=int(input("Enter the number of terms"))
+first=0                                         
+second=1                                         
+if a<=0:
+    print("The requested series is",f)
+else:
+    print(first,second,end=" ")
+    for x in range(2,a):
+        next=first+second                           
+        print(next,end=" ")
+        first=second
+        second=next
